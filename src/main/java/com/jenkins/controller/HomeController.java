4b4582ef9d30f5eac59fae5cb6f69a9b5d60464c
ankit-2210 <78948475+ankit-2210@users.noreply.github.com>
2026-05-03 15:53:29 +0530

    changes made in controller

--- src/main/java/com/jenkins/controller/HomeController.java
+++ src/main/java/com/jenkins/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String HomeControllerHandler(){
-        return "Jenkins microservices for integration";
+        return "Jenkins microservices for integration.git";
     }
 
 }
